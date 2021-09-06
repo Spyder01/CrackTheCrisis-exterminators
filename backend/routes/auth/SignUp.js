60f1c32a5e173c1eec17edf5de5788131e7f255c
spyder01 <suhan01.bangera@gmail.com>
2021-09-06 15:03:09 +0530

    Just random stuff

--- backend/routes/auth/SignUp.js
+++ backend/routes/auth/SignUp.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const Users = require('../../db/Model');
 const app = express.Router();
+
 
 app.get('/sign-up', (req, res) => {
     res.send ('<h1>Hello, World!!</h1>');
 })
 
-app.post('/sign-up', (req, res)=>{
+app.post('/sign-up', async (req, res)=>{
     const Info = req.body;
-
+    console.table(Info)
+    const user = await Users.findOne({ email: Info.email });
+    if(!user) console.log("hi there")
+     
 })
 
 
