7b37c42bac803ab80b1dde34d59258774c9b1b87
spyder01 <suhan01.bangera@gmail.com>
2021-09-06 15:58:20 +0530

    Merge conflicts

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const BodyParser = require('body-parser');
-
+const cors = require('cors');
+const SignUp = require('./routes/auth/SignUp')
+const Socket = require('./routes/socket/sockets')
 
 
 
@@ -10,13 +12,16 @@ dotenv.config();
 
 const app = express ();
 app.use(BodyParser.json())
+app.use(cors())
 const PORT = process.env.PORT || 5000;
-
-
 
 app.get('/', (req, res)=>{
     res.send("<h1>Hello, World!!</h1>")
 })
+
+app.use('/auth', SignUp)
+app.use('/socket', Socket);
+
 
 const DataBaseAuth = `mongodb+srv://Suhan:${process.env.MONGO_PASSWORD}@exterminators.7pup1.mongodb.net/Exterminators?retryWrites=true&w=majority`
 mongoose.connect(DataBaseAuth, {useNewUrlParser: true, useUnifiedTopology: true}).then((res) => {
